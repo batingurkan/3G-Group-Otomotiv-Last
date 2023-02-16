d13aee481974019ec166abf6aab1a0440a10fffb
Batın Gürkan <batingurkan01@gmail.com>
2023-02-16 13:37:09 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using _3G_Group_Otomotiv.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3G_Group_Otomotiv.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public IActionResult Kurumsal()
+        {
+            return View();
+        }
+
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+        public IActionResult Tedarikcilerimiz()
+        {
+            return View();
+        }
+        public IActionResult Bayibasvuru()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _3G_Group_Otomotiv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
